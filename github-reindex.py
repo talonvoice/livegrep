88b9c2415f3a30456f9089d4e2bd0ce982182a7b
Ryan Hileman <lunixbochs@gmail.com>
2021-04-28 10:18:16 -0700

    add python github reindex script

---
+++ github-reindex.py
@@ -0,0 +1,125 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
+from urllib.parse import urlparse
+import itertools
+import json
+import os
+import requests
+import sys
+
+@dataclass
+class Fork:
+    user:     str
+    repo:     str
+    url:      str
+    http_url: str
+    git_url:  str
+    ssh_url:  str
+    stars:    int
+
+    @classmethod
+    def parse(cls, j):
+        return cls(
+            user=j["owner"]["login"],
+            repo=j["name"],
+            url=j["html_url"],
+            http_url=j["clone_url"],
+            git_url=j["git_url"],
+            ssh_url=j["ssh_url"],
+            stars=j["stargazers_count"],
+        )
+
+def await_rate_limit():
+    r = requests.get("https://api.github.com/rate_limit")
+    r.raise_for_status()
+    j = r.json()
+    core = j["resources"]["core"]
+    if core["remaining"] == 0:
+        time.sleep(min(3600, core["reset"] - time.time() + 5))
+
+def is_rate_limited(r):
+    return r.headers.get("X-RateLimit-Remaining") == "0"
+
+def fetch(url, auth=()):
+    for i in range(3):
+        r = requests.get(url, auth=auth)
+        if r.status_code == 200:
+            break
+        if r.status_code == 403 and is_rate_limited(r):
+            await_rate_limit()
+        else:
+            r.raise_for_status()
+    else:
+        r.raise_for_status()
+    return r
+
+def get_forks(user, repo, auth=()):
+    await_rate_limit()
+    r = fetch(f"https://api.github.com/repos/{user}/{repo}", auth=auth)
+    j = r.json()
+    yield Fork.parse(j)
+
+    if not j.get('forks_count'):
+        return
+
+    for page in itertools.count():
+        r = fetch(f"https://api.github.com/repos/{user}/{repo}/forks?page={page}", auth=auth)
+        j = r.json()
+        if not j:
+            break
+        for item in j:
+            yield Fork.parse(item)
+        if is_rate_limited(r):
+            await_rate_limit()
+
+def parse_url(url):
+    if not "://" in url:
+        url = f"https://{url}"
+    p = urlparse(url)
+    if p.netloc != "github.com":
+        print(f"[!] skipping unknown host: {url}", file=sys.stderr)
+        return None, None
+    path = p.path.strip("/")
+    user, repo, *extra = path.split("/")
+    return user, repo
+
+def build_config(name, path, urls):
+    fpath = Path(path)
+    forks = []
+    for url in urls:
+        user, repo = parse_url(url)
+        if not repo:
+            continue
+        for fork in get_forks(user, repo):
+            forks.append(fork)
+    forks.sort(key=lambda x: x.stars, reverse=True)
+
+    config = {
+        "name": name,
+        "repos": [],
+    }
+    repos = config["repos"]
+    for fork in forks:
+        repos.append({
+            "path": str(fpath / fork.user / fork.repo),
+            "name": f"{fork.user}/{fork.repo}",
+            "revisions": ["HEAD"],
+            "metadata": {
+                "remote": fork.http_url,
+                "github": fork.url,
+            },
+        })
+    with open(fpath / "livegrep.json", "w") as f:
+        json.dump(config, f, indent=4)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: livegrep-index <path> <name> <github url> ...", file=sys.stderr)
+        sys.exit(1)
+
+    name = sys.argv[1]
+    path = sys.argv[2]
+    urls = sys.argv[3:]
+    os.makedirs(path, exist_ok=True)
+    build_config(name, path, urls)
